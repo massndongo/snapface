cab43aea6de8ddc939d316e6e44abb977f8c12e1
massndongo86@gmail.com <massndongo86@gmail.com>
2023-10-09 16:41:11 +0000

    Pipe

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss']
+})
+export class FaceSnapComponent implements OnInit {
+  @Input() faceSnap!: FaceSnap;
+  buttonText!: string;
+
+
+  ngOnInit() {
+    this.buttonText = "Oh snap!";
+  }
+
+  onSnap(){
+    if (this.buttonText === "Oh snap!") {
+      this.faceSnap.snaps++;
+      this.buttonText = "Oops, unsnap!";
+    } else {
+      this.faceSnap.snaps--;
+      this.buttonText = "Oh snap!";
+    }
+  }
+
+}
---
+++ src/app/models/face-snap.model.ts
@@ -0,0 +1,19 @@
+export  class FaceSnap {
+    title!: string;
+    description!: string;
+    imageUrl!: string;
+    createdDate!: Date;
+    snaps!: number;
+    location?: string;
+  }
+
+  // constructor(
+  //   public title: string,
+  //   public description: string,
+  //   public createdDate: Date,
+  //   public snaps: number,
+  //   public imageUrl: string
+  //   ) {
+
+  // }
+
