2eae979cd0d940b293d3bddb45ef32a3900c35ff
massndongo86@gmail.com <massndongo86@gmail.com>
2023-10-10 16:03:04 +0000

    Observable | Unsubscribe

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
 import { FaceSnapsService } from '../services/face-snaps.service';
+import * as internal from 'stream';
+import { Subject, interval, take, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'app-face-snap-list',
   templateUrl: './face-snap-list.component.html',
   styleUrls: ['./face-snap-list.component.scss']
 })
-export class FaceSnapListComponent implements OnInit {
+export class FaceSnapListComponent implements OnInit, OnDestroy {
 
   faceSnaps!: FaceSnap[];
+  private destroy$!: Subject<boolean>;
 
   constructor(private faceSnapsService: FaceSnapsService) {
 
@@ -17,5 +20,15 @@ export class FaceSnapListComponent implements OnInit {
 
   ngOnInit() {
     this.faceSnaps = this.faceSnapsService.getAllFaceSnaps();
+
+    this.destroy$ = new Subject<boolean>;
+    interval(1000).pipe(
+      takeUntil(this.destroy$),
+      tap(console.log)
+    ).subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
   }
 }
